af2cc980f475cb88711778b6ee486bc0b84dcf46
arthur <118621989+arthurito276@users.noreply.github.com>
2022-11-23 09:21:26 +0100

    Ajout des fichiers de départ

---
+++ fichiers python/toit1.py
@@ -0,0 +1,19 @@
+import turtle
+from trait import trait
+
+def toit1(x, ySol, niveau):
+    '''
+    Paramètres :
+        x : abcisse du centre du toit
+        ySol : ordonnée du sol du la rue
+        niveau : num du niveau (0 pour les rdc, ...)
+    Cette fonction dessine un toit triangulaire noir de base 160 pixels
+    et de hauteur centrale 40 pixels
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    toit1(0,0,0)
+    # On ferme la fenêtre s'il y a un clic gauche
+    turtle.exitonclick()
