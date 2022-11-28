08a194aa0ba6812c32b74157dea422a4b3158be8
Arthur <arthur.cott@free.fr>
2022-11-28 21:59:24 +0100

    Correction : vitesse tortue et tortue masquée

--- python final/trait.py
+++ python final/trait.py
@@ -7,6 +7,8 @@ def trait(x1,y1,x2,y2):
         x2, y2 : coordonnées de la fin du trait
     Cette function dessine un trait entre les 2 points transmis en paramètres
     '''
+    turtle.hideturtle()
+    turtle.speed(0)
     turtle.penup()
     turtle.goto(x1,y1)
     turtle.pendown()
