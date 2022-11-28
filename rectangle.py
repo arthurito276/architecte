a3cbdcb112f738a1d40c626e70f2296c2a4ba999
Arthur <arthur.cott@free.fr>
2022-11-28 16:32:52 +0100

    fonction rectangle terminée

---
+++ rectangle.py
@@ -0,0 +1,23 @@
+import turtle
+from trait import trait
+
+
+def rectangle(x,y,w,h):
+    '''
+    Paramètres
+        x, y : coordonnées du centre de la base de rectangle
+        w : largeur du rectangle
+        h : hauteur du rectangle
+    Cette fonction dessine un rectangle Le point de coordonnées (x,y) est
+    sur le côté en bas au milieu
+    '''
+    trait(x-w/2, y, x+w/2, y)
+    trait(x-w/2, y, x-w/2, y+h)
+    trait(x-w/2, y+h, x+w/2, y+h)
+    trait(x+w/2, y+h, x+w/2, y)
+
+
+if __name__ == '__main__':
+    rectangle(0,0,150,100)
+    # On ferme la fenêtre s'il y a un clique gauche
+    turtle.exitonclick()
