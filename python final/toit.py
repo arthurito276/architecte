0cdee0b0fc74126757de67da0a147265dc855dba
Mastube <maxime.dugue25@gmail.com>
2022-11-30 09:37:07 +0100

    toit fini

---
+++ python final/toit.py
@@ -0,0 +1,25 @@
+from random import randint
+from toit1 import toit1
+from toit2 import toit2
+import turtle
+
+def toit(x, ySol, niveau):
+    '''
+    Paramètres
+        x : abscisse du centre de l'étage
+        ySol: ordonnée du sol
+        niveau : numéro de l'étage en partant de 0 pour le rdc
+    Cette fonction dessine au hasard un des 2 types de toit
+
+    '''
+    toit = randint(1,2)
+    if toit == 1:
+        toit1(x,ySol,niveau)
+    else:
+        toit2(x,ySol,niveau)
+
+
+if __name__ == '__main__':
+    toit(70,20,1)
+    # On ferme la fenêtre s'il y a un clique gauche
+    turtle.exitonclick()
