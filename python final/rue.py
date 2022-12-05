0a4bb05a8a39e94ff36f27d622464dce92a56746
Arthur <arthur.cott@free.fr>
2022-12-05 22:26:39 +0100

    fini rue et correction sol

---
+++ python final/rue.py
@@ -0,0 +1,29 @@
+import turtle
+from random import randint, shuffle
+from sol import sol
+from immeuble import immeuble
+# ------------------------------
+# ------------------------------
+# ------------------------------
+
+def main():
+    turtle.setup(800, 600)
+    turtle.speed(2)
+    # On définit la hauteur du sol
+    y_sol = -200
+    # Dessin du sol de la rue
+    sol(y_sol)
+    # Dessin des 4 immeubles
+    immeuble(-282,y_sol)
+    immeuble(-94,y_sol)
+    immeuble(94,y_sol)
+    immeuble(282,y_sol)
+
+    # On ferme la fenêtre s'il y a un clique gauche
+    turtle.exitonclick()
+
+if __name__ == '__main__':
+    main()
+
+
+
