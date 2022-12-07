1a95deebfcb4c1c1003d454de23048020e929828
Arthur <arthur.cott@free.fr>
2022-12-07 13:36:44 +0100

    Fini lampadaire test

--- python final/lampadaire.py
+++ python final/lampadaire.py
@@ -1,8 +1,20 @@
 from rectangle import rectangle
 import turtle
+
 from trait import trait
 def lampadaire(x,y_sol):
     turtle.fillcolor("black")
     turtle.begin_fill()
-
     rectangle(x,y_sol,5,90)
+    turtle.end_fill()
+    
+    turtle.pencolor("gold")
+    turtle.fillcolor("gold")
+    turtle.begin_fill()
+    rectangle(x,y_sol+80,15,15)
+    turtle.end_fill()
+    turtle.pencolor("black")
+
+if __name__ == "__main__":
+    lampadaire(45,0)
+    turtle.exitonclick()
