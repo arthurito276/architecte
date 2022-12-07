f8d3ebd44e9f92cd94bd6b006a9801ec1e7eb6db
Mastube <maxime.dugue25@gmail.com>
2022-12-07 09:46:45 +0100

    commencé lampadaire

---
+++ python final/lampadaire.py
@@ -0,0 +1,8 @@
+from rectangle import rectangle
+import turtle
+from trait import trait
+def lampadaire(x,y_sol):
+    turtle.fillcolor("black")
+    turtle.begin_fill()
+
+    rectangle(x,y_sol,5,90)
