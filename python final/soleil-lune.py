5243d4209981912342378194afc60e3e54d865ea
Arthur <arthur.cott@free.fr>
2022-12-12 21:31:51 +0100

    fini soleil lune

--- python final/soleil-lune.py
+++ python final/soleil-lune.py
@@ -5,9 +5,14 @@ import datetime as dt
 
 def astre():
     heure = dt.datetime.now().hour
+    x_astre = (800/12)*(heure-12)
+    y_astre = -1*(heure-12)**2+250
+    print(x_astre,y_astre)
     if heure > 8 and heure < 20:
-        x_astre = (800/12)*(heure-12)
-        y_astre = x_astre**2
+        rectangle(x_astre,y_astre,40,40,"gold")
+    else:
+        rectangle(x_astre,y_astre,40,40,"grey")
 
 if __name__ == "__main__":
     astre()
+    turtle.exitonclick()
